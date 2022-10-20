1a1d4280af68d5e98e4c6a91a0d1e06307177e67
Noah <1627295329@qq.com>
2022-10-21 01:54:32 +0800

    feat: add loading animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import type { Component } from 'solid-js';
-import { createSignal } from 'solid-js';
+import { createSignal, Component } from 'solid-js';
 import axios from 'axios';
 
 // 防抖
@@ -49,9 +48,13 @@ const defaultAnswerData: AnswerData = {
 
 // 答案数据
 const [answerData, setAnswerData] = createSignal<AnswerData>(defaultAnswerData);
+// 加载
+const [loading, setLoading] = createSignal(false);
 
 // 获取答案
 const handleGetAnswer = debounce(async (question: string) => {
+  // 加载中
+  setLoading(true);
   const res = await axios.post<ResponseData>(
     'https://api.answer.uu988.xyz/answer/search',
     JSON.stringify({ question }),
@@ -62,6 +65,8 @@ const handleGetAnswer = debounce(async (question: string) => {
     }
   );
   const { data, errno } = res.data;
+  // 加载完成
+  setLoading(false);
   if (errno !== -1) {
     return data;
   }
@@ -103,14 +108,40 @@ const App: Component = () => {
         />
       </div>
       <div class="px-5 py-10 flex justify-center">
-        <div class="bg-white bg-opacity-50 rounded-md px-3 py-2 backdrop-blur max-w-xl w-4/5 min-w-[20rem]">
+        <div
+          class={`bg-white bg-opacity-50 rounded-md px-3 py-2 backdrop-blur max-w-xl w-4/5 min-w-[20rem] ${
+            loading() ? 'animate-pulse' : ''
+          }`}
+        >
           <div class="text-blue-500">
-            <div class="py-1">
+            <div class="py-1 flex justify-between">
               <span class="text-white rounded-lg bg-blue-400 px-2 py-0.5 text-xs">
                 <span class="">{questionTypes[answerData().type]}</span>
                 <span class="px-1">/</span>
                 <span class="capitalize opacity-80">{answerData().type}</span>
               </span>
+              {loading() && (
+                <svg
+                  class="animate-spin h-5 w-5 text-blue-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    class="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    stroke-width="4"
+                  ></circle>
+                  <path
+                    class="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+              )}
             </div>
             <div class="py-1">
               <span>答案：</span>
